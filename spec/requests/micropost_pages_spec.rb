5b67596d178231e42da85d55a0a7f3be63870cde
Jason Benn <jasonbenn@outlook.com>
2013-04-29 12:53:49 -0400

    Add destroy link to microposts

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -8,7 +8,7 @@ describe "Micropost pages" do
   before { sign_in user }
 
   describe "micropost creation" do
-  before { visit root_path }
+    before { visit root_path }
 
     describe "with invalid information" do
       
@@ -28,6 +28,18 @@ describe "Micropost pages" do
       it "should create a micropost" do
         expect { click_button "Post" }.should change(Micropost, 
           :count).by(1)
+      end
+    end
+  end
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, content: "a", user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should reduce micropost count by 1" do
+        expect { click_link "delete" }.should change(Micropost, :count).by(-1)
       end
     end
   end
